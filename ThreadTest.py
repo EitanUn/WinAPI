9543f06d27770a1de31c7674d942fc5b603047e6
highlord69 <eitanung@gmail.com>
2022-12-14 15:12:55 +0200

    Threads implemented

--- ThreadTest.py
+++ ThreadTest.py
@@ -1,21 +1,23 @@
 from syncdb import SyncDB
 from filedb import FileDB
-import threading
+import win32process
+from win32event import WaitForSingleObject as Join, INFINITE
 import logging
 
 FORMAT = '%(asctime)s.%(msecs)03d - %(message)s'
 DATEFMT = '%H:%M:%S'
+STACK_SIZE = 1000
 
 
 def test_write(db):
     logging.debug("started write test")
-    for i in range(1000):
+    for i in range(100):
         assert db.set_value(i, "test" + str(i))
 
 
 def test_read(db):
     logging.debug("started read test")
-    for i in range(1000):
+    for i in range(100):
         assert "test" + str(i) == db.get_value(i)
 
 
@@ -24,63 +26,53 @@ def main():
     db = SyncDB(FileDB())
     logging.debug("\n--------------------------------------------------------\n")
     logging.info("testing simple write perms")
-    p1 = threading.Thread(target=test_write, args=(db, ))
-    p1.start()
-    p1.join()
+    p1 = win32process.beginthreadex(None, STACK_SIZE, test_write, (db, ), 0)[0]
+    assert Join(p1, INFINITE) == 0
     logging.info("test successful")
     logging.debug("\n--------------------------------------------------------\n")
     logging.info("testing simple read perms")
-    p1 = threading.Thread(target=test_read, args=(db, ))
-    p1.start()
-    p1.join()
+    p1 = win32process.beginthreadex(None, STACK_SIZE, test_read, (db, ), 0)[0]
+    assert Join(p1, INFINITE) == 0
     logging.info("test successful")
     logging.debug("\n--------------------------------------------------------\n")
     logging.info("testing read blocks writing")
-    p1 = threading.Thread(target=test_read, args=(db, ))
-    p2 = threading.Thread(target=test_write, args=(db, ))
-    p1.start()
-    p2.start()
-    p1.join()
-    p2.join()
+    p1 = win32process.beginthreadex(None, STACK_SIZE, test_read, (db, ), 0)[0]
+    p2 = win32process.beginthreadex(None, STACK_SIZE, test_write, (db, ), 0)[0]
+    assert Join(p1, INFINITE) == 0
+    assert Join(p2, INFINITE) == 0
     logging.info("test successful")
     logging.debug("\n--------------------------------------------------------\n")
     logging.info("testing write blocks reading")
-    p1 = threading.Thread(target=test_write, args=(db, ))
-    p2 = threading.Thread(target=test_read, args=(db, ))
-    p1.start()
-    p2.start()
-    p1.join()
-    p2.join()
+    p1 = win32process.beginthreadex(None, STACK_SIZE, test_write, (db, ), 0)[0]
+    p2 = win32process.beginthreadex(None, STACK_SIZE, test_read, (db, ), 0)[0]
+    assert Join(p1, INFINITE) == 0
+    assert Join(p2, INFINITE) == 0
     logging.info("test successful")
     logging.debug("\n--------------------------------------------------------\n")
     logging.info("testing multi reading perms possible")
     threads = []
     for i in range(5):
-        thread = threading.Thread(target=test_read, args=(db, ))
-        thread.start()
+        thread = win32process.beginthreadex(None, STACK_SIZE, test_read, (db, ), 0)[0]
         threads.append(thread)
     for i in threads:
-        i.join()
+        assert Join(i, INFINITE) == 0
     logging.info("test successful")
     logging.debug("\n--------------------------------------------------------\n")
     logging.info("testing load")
     threads = []
     for i in range(15):
-        thread = threading.Thread(target=test_read, args=(db, ))
-        thread.start()
+        thread = win32process.beginthreadex(None, STACK_SIZE, test_read, (db, ), 0)[0]
         threads.append(thread)
     for i in range(5):
-        p1 = threading.Thread(target=test_write, args=(db,))
-        p1.start()
+        p1 = win32process.beginthreadex(None, STACK_SIZE, test_write, (db, ), 0)[0]
         threads.append(p1)
     for i in threads:
-        i.join()
+        assert Join(i, INFINITE) == 0
     logging.info("test successful")
     logging.debug("\n--------------------------------------------------------\n")
     logging.info("testing values stay correct")
-    p1 = threading.Thread(target=test_read, args=(db,))
-    p1.start()
-    p1.join()
+    p1 = win32process.beginthreadex(None, STACK_SIZE, test_read, (db, ), 0)[0]
+    assert Join(p1, INFINITE) == 0
     logging.info("test successful")
 
 
